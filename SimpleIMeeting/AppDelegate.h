b3ed1c80e3d4d26a138388f015ee128bf7803a20
chelsea zhai <chelsea.zhai@gmail.com>
2013-05-27 09:14:03 +0800

    Initial Commit

---
+++ SimpleIMeeting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleIMeeting
+//
+//  Created by Ares on 13-5-27.
+//  Copyright (c) 2013年 richitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
